4a6980f9eba5aa1ec6b75d57efc2cc13083c5424
highxtrtys <highxtrtys@gmail.com>
2025-01-08 19:36:03 +0100

    Welcome and page file added

---
+++ page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+<form action="welcome_get.php" method="GET">
+    User: <input type="text" name="name">
+    pw: <input type="password" name="password">
+    <input type="submit">
+</form>
+</body>
+</html>
---
+++ welcome_get.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your corresponding pw is: <?php echo $_GET["password"]; ?>
+
+<form action="page.php">
+    <input type="submit" value="return">
+</form>
+</body>
+</html>
